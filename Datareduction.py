8d012ddc7c020848e739887b0ba115e5ab3b7d5a
jalon360 <85844170+jalon360@users.noreply.github.com>
2023-09-21 15:20:42 -0400

    Update and rename Datareduction to Datareduction.py

---
+++ Datareduction.py
@@ -0,0 +1,56 @@
+import csv
+from ObjectDetector import ObjectDetector
+from Util import util
+class ImageDataProcessor:
+    def __init__(self, binary_image_data):
+        self.data = binary_image_data
+
+    def getTimestamp(self):
+        # Code to extract and return the timestamp from binary_image_data
+        # Assuming you have a timestamp extraction method
+        timestamp = extract_timestamp(binary_image_data)  # Replace with your method
+        return timestamp
+
+    def getMinAmplitude(self):
+        # Code to calculate and return the minimum amplitude of the signal
+        min_amplitude = min(self.data)  # Assuming data is a list of amplitudes
+        return min_amplitude
+
+    def getMaxAmplitude(self):
+        # Code to calculate and return the maximum amplitude of the signal
+        max_amplitude = max(self.data)  # Assuming data is a list of amplitudes
+        return max_amplitude
+
+    def getAverageAmplitude(self):
+        # Code to calculate and return the average amplitude of the signal
+        average_amplitude = sum(self.data) / len(self.data)  # Assuming data is a list of amplitudes
+        return average_amplitude
+
+    def writeData(self, output_file):
+        # Assuming extracted necessary data from the video
+        # previously defined utilities like util.getCenter(), etc.
+        center_frequency = util.getCenter('path_to_image')  # Replace with actual image path
+        timestamp = self.getTimestamp()
+        min_amplitude = self.getMinAmplitude()
+        max_amplitude = self.getMaxAmplitude()
+        average_amplitude = self.getAverageAmplitude()
+
+        # Write the reduced data to a CSV file
+        with open(output_file, 'w', newline='') as csvfile:
+            fieldnames = ['Timestamp', 'Center Frequency', 'Min Amplitude', 'Max Amplitude', 'Average Amplitude']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerow({
+                'Timestamp': timestamp,
+                'Center Frequency': center_frequency,
+                'Min Amplitude': min_amplitude,
+                'Max Amplitude': max_amplitude,
+                'Average Amplitude': average_amplitude
+            })
+
+# Function to extract timestamp from binary_image_data (replace with actual logic)
+def extract_timestamp(binary_image_data):
+    # Your timestamp extraction logic here
+    timestamp = '2023-09-19 12:34:56'  # Replace with actual timestamp
+    return timestamp

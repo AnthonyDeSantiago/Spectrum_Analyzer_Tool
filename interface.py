595499ca868ce6c0fe4615f3a267897eaa2b6fdf
Hurkaleez <matthew.morgan105@gmail.com>
2023-11-13 20:54:47 -0500

    Button

--- interface.py
+++ interface.py
@@ -1,6 +1,8 @@
 
 import sys
+import time
 import io
+import asyncio
 from contextlib import redirect_stdout
 import main
 from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog, QInputDialog, QGraphicsScene, QGraphicsView
@@ -33,7 +35,8 @@ class Main(QMainWindow):
         self.minPowerButton.clicked.connect(self.set_min_power)
         self.maxPowerButton.clicked.connect(self.set_max_power)
 
-        self.script1Button.clicked.connect(self.call_main)
+        self.script1Button.clicked.connect(self.call_model1)
+        self.script2Button.clicked.connect(self.call_model2)
 
         self.imgGotoFirst.clicked.connect(self.set_image_first)
         self.imgGotoPrevious.clicked.connect(self.set_image_previous)
@@ -41,19 +44,6 @@ class Main(QMainWindow):
         self.imgGotoLast.clicked.connect(self.set_image_last)
 
         self.resetLog.clicked.connect(self.reset_log)
-
-
-        # Temp code for proof of images-------------------------------
-        # scene = QGraphicsScene(0, 0, 400, 200)
-
-        # pixmap = QPixmap("8x9u9cS.jpg")
-        # pixmapitem = scene.addPixmap(pixmap)
-        # pixmapitem.setPos(10, 10)
-
-        # self.graphicsView.setScene(scene)
-        # self.graphicsView.setRenderHint(QPainter.RenderHint.Antialiasing)
-        # self.graphicsView.show()
-        # Temp code for proof of images-------------------------------
 
     @pyqtSlot()
     def open_dialog(self):
@@ -74,7 +64,11 @@ class Main(QMainWindow):
 
         self.eventLog.appendPlainText("Video Selected: " + filePath)
 
-        return filePath
+        self.centerButton.setEnabled(True)
+        self.referenceButton.setEnabled(True)
+        self.spanButton.setEnabled(True)
+        self.minPowerButton.setEnabled(True)
+        self.maxPowerButton.setEnabled(True)
 
     def set_center(self):
         d, ok = QInputDialog().getDouble(self, "Center Frequency",
@@ -84,6 +78,7 @@ class Main(QMainWindow):
         if ok:
             self.label_6.setText("Center: "+ str(d) + " GHz")
             main.center_frequency = d
+            self.eventLog.appendPlainText("Center Frequency Entered: " + str(d))
         else:
             self.label_6.setText("No Center Selected!")
 
@@ -95,6 +90,7 @@ class Main(QMainWindow):
         if ok:
             self.label_7.setText("Reference: "+ str(d) +" dBm")
             main.reference_level = d
+            self.eventLog.appendPlainText("Reference Level Entered: " + str(d))
         else:
             self.label_7.setText("No Reference Selected!")
 
@@ -106,6 +102,7 @@ class Main(QMainWindow):
         if ok:
             self.label_8.setText("Span: "+ str(d) +" MHz")
             main.span = d
+            self.eventLog.appendPlainText("Span Entered: " + str(d))
         else:
             self.label_8.setText("No Span Selected!")
 
@@ -117,6 +114,7 @@ class Main(QMainWindow):
         if ok:
             self.label_9.setText("Minimum Power: "+ str(d) +" dB/")
             main.min_power = d
+            self.eventLog.appendPlainText("Minimum Power Entered: " + str(d))
         else:
             self.label_9.setText("No Minimum Power Selected!")
 
@@ -128,31 +126,78 @@ class Main(QMainWindow):
             if ok:
                 self.maxPowerOutput.setText("Maximum Power: "+ str(d) +" dB/")
                 main.max_power = d
+                self.eventLog.appendPlainText("Maximum Power Entered: " + str(d))
             else:
                 self.maxPowerOutput.setText("No Maximum Power Selected!")
 
-    def call_main(self):
-        self.scriptStatus.setText("Processing Video...")
-        with redirect_stdout(str_out):  
-            main.script_main()
+    def call_model1(self):
 
-        self.scriptStatus.setText("Video Processed! CSV file has been added to directory")
+        self.scriptStatus.setText("Processing Video...")
+        self.repaint()
+
+        with redirect_stdout(str_out):
+            # QApplication.setOverrideCursor(pyqtSlot.WaitCursor)
+            main.script_trained_ml_approach()
+            # QApplication.restoreOverrideCursor()
 
         out = str_out.getvalue()
         self.eventLog.appendPlainText(out)
         str_out.truncate(0)
 
-        # self.eventLog.appendPlainText('Clicked on First Button')
+        self.scriptStatus.setText("Video Processed! CSV file has been added to directory")
+
+        self.imgGotoFirst.setEnabled(True)
+        self.imgGotoPrevious.setEnabled(True)
+        self.imgGotoNext.setEnabled(True)
+        self.imgGotoLast.setEnabled(True)
+
         scene = QGraphicsScene(0, 0, 0, 0)
 
-        Main.image_index = 0 
+        Main.image_index = 0
         image_prop = Main.image_array[Main.image_index]
         self.imgTitle.setText(image_prop[0].split(',')[0])
         image_path = image_prop[0].split(',')[1]
-        # pixmap = QPixmap(image_path)
-        # pixmap.scaled(s[, aspectMode=Qt.IgnoreAspectRatio[, mode=Qt.FastTransformation]])
-        # pixmapitem = scene.addPixmap(pixmap)
-        #pixmapitem.setPos(0, 0)
+        pixmap = QPixmap(image_path)
+        pixmapitem = scene.addPixmap(pixmap)
+        pixmapitem.setPos(0, 0)
+
+        self.graphicsView.setScene(scene)
+        self.graphicsView.setRenderHint(QPainter.RenderHint.Antialiasing)
+        self.graphicsView.show()
+
+        image_number = Main.image_index + 1
+        total_image_number = len(Main.image_array)
+        self.label_5.setText("Image: "+ str(image_number) + " / " + str(total_image_number))
+
+    def call_model2(self):
+        self.scriptStatus.setText("Processing Video...")
+        self.repaint()
+
+        with redirect_stdout(str_out):
+            # QApplication.setOverrideCursor(pyqtSlot.WaitCursor)
+            main.script_main()
+            # QApplication.restoreOverrideCursor()
+
+        out = str_out.getvalue()
+        self.eventLog.appendPlainText(out)
+        str_out.truncate(0)
+
+        self.scriptStatus.setText("Video Processed! CSV file has been added to directory")
+
+        self.imgGotoFirst.setEnabled(True)
+        self.imgGotoPrevious.setEnabled(True)
+        self.imgGotoNext.setEnabled(True)
+        self.imgGotoLast.setEnabled(True)
+
+        scene = QGraphicsScene(0, 0, 0, 0)
+
+        Main.image_index = 0
+        image_prop = Main.image_array[Main.image_index]
+        self.imgTitle.setText(image_prop[0].split(',')[0])
+        image_path = image_prop[0].split(',')[1]
+        pixmap = QPixmap(image_path)
+        pixmapitem = scene.addPixmap(pixmap)
+        pixmapitem.setPos(0, 0)
 
         self.graphicsView.setScene(scene)
         self.graphicsView.setRenderHint(QPainter.RenderHint.Antialiasing)
@@ -163,19 +208,20 @@ class Main(QMainWindow):
         self.label_5.setText("Image: "+ str(image_number) + " / " + str(total_image_number))
 
 
-
+    async def runModels():
+        await asyncio.run(main.script_main)
+ 
     def set_image_first(self):
-        # self.eventLog.appendPlainText('Clicked on First Button')
         scene = QGraphicsScene(0, 0, 0, 0)
 
-        Main.image_index = 0 
+        Main.image_index = 0
         image_prop = Main.image_array[Main.image_index]
         self.imgTitle.setText(image_prop[0].split(',')[0])
         image_path = image_prop[0].split(',')[1]
         pixmap = QPixmap(image_path)
         # pixmap.scaled(s[, aspectMode=Qt.IgnoreAspectRatio[, mode=Qt.FastTransformation]])
         pixmapitem = scene.addPixmap(pixmap)
-        #pixmapitem.setPos(0, 0)
+        pixmapitem.setPos(0, 0)
 
         self.graphicsView.setScene(scene)
         self.graphicsView.setRenderHint(QPainter.RenderHint.Antialiasing)
@@ -187,60 +233,106 @@ class Main(QMainWindow):
             
 
     def set_image_previous(self):
-        # self.eventLog.appendPlainText('Clicked on Previous Button')
         scene = QGraphicsScene(0, 0, 0, 0)
 
         Main.image_index = Main.image_index - 1 
-        image_prop = Main.image_array[Main.image_index]
-        self.imgTitle.setText(image_prop[0].split(',')[0])
-        image_path = image_prop[0].split(',')[1]
-        pixmap = QPixmap(image_path)
-        pixmapitem = scene.addPixmap(pixmap)
+        
+        if (Main.image_index < 0):
+            Main.image_index = 0
 
-        self.graphicsView.setScene(scene)
-        self.graphicsView.setRenderHint(QPainter.RenderHint.Antialiasing)
-        self.graphicsView.show()
+            image_prop = Main.image_array[Main.image_index]
+            self.imgTitle.setText(image_prop[0].split(',')[0])
 
-        image_number = Main.image_index + 1
-        total_image_number = len(Main.image_array)
-        self.label_5.setText("Image: "+ str(image_number) + " / " + str(total_image_number))
+            image_path = image_prop[0].split(',')[1]
+            pixmap = QPixmap(image_path)
+            pixmapitem = scene.addPixmap(pixmap)
+            pixmapitem.setPos(0, 0)
+
+            self.graphicsView.setScene(scene)
+            self.graphicsView.setRenderHint(QPainter.RenderHint.Antialiasing)
+            self.graphicsView.show()
+
+            image_number = Main.image_index + 1
+            total_image_number = len(Main.image_array)
+            self.label_5.setText("Image: "+ str(image_number) + " / " + str(total_image_number))
+
+        else:
+            image_prop = Main.image_array[Main.image_index]
+            self.imgTitle.setText(image_prop[0].split(',')[0])
+
+            image_path = image_prop[0].split(',')[1]
+            pixmap = QPixmap(image_path)
+            pixmapitem = scene.addPixmap(pixmap)
+            pixmapitem.setPos(0, 0)
+
+            self.graphicsView.setScene(scene)
+            self.graphicsView.setRenderHint(QPainter.RenderHint.Antialiasing)
+            self.graphicsView.show()
+
+            image_number = Main.image_index + 1
+            total_image_number = len(Main.image_array)
+            self.label_5.setText("Image: "+ str(image_number) + " / " + str(total_image_number))
 
     def set_image_next(self):
-        # self.eventLog.appendPlainText('Clicked on Next Button')
+
+        if (Main.image_index < len(Main.image_array) - 1):
+            scene = QGraphicsScene(0, 0, 0, 0)
+            Main.image_index = Main.image_index + 1
+
+            # if (Main.image_index > len(Main.image_array) + 1):
+            # image_index = len(Main.image_array) - 1
+
+            image_prop = Main.image_array[Main.image_index]
+            self.imgTitle.setText(image_prop[0].split(',')[0])
+
+            image_path = image_prop[0].split(',')[1]
+            pixmap = QPixmap(image_path)
+            pixmapitem = scene.addPixmap(pixmap)
+            pixmapitem.setPos(0, 0)
+
+            self.graphicsView.setScene(scene)
+            self.graphicsView.setRenderHint(QPainter.RenderHint.Antialiasing)
+            self.graphicsView.show()
+
+            image_number = Main.image_index + 1
+            total_image_number = len(Main.image_array)
+            self.label_5.setText("Image: "+ str(image_number) + " / " + str(total_image_number))
+
+            # else:
+            #     image_prop = Main.image_array[Main.image_index]
+            #     self.imgTitle.setText(image_prop[0].split(',')[0])
+
+            #     image_path = image_prop[0].split(',')[1]
+            #     pixmap = QPixmap(image_path)
+            #     pixmapitem = scene.addPixmap(pixmap)
+            #     pixmapitem.setPos(0, 0)
+
+            #     self.graphicsView.setScene(scene)
+            #     self.graphicsView.setRenderHint(QPainter.RenderHint.Antialiasing)
+            #     self.graphicsView.show()
+
+            #     image_number = Main.image_index + 1
+            #     total_image_number = len(Main.image_array)
+            #     self.label_5.setText("Image: "+ str(image_number) + " / " + str(total_image_number))
+
+    def set_image_last(self):
         scene = QGraphicsScene(0, 0, 0, 0)
 
-        Main.image_index = Main.image_index + 1
+        Main.image_index = len(Main.image_array) - 1
 
         image_prop = Main.image_array[Main.image_index]
         self.imgTitle.setText(image_prop[0].split(',')[0])
+
         image_path = image_prop[0].split(',')[1]
         pixmap = QPixmap(image_path)
         pixmapitem = scene.addPixmap(pixmap)
+        pixmapitem.setPos(0, 0)
 
         self.graphicsView.setScene(scene)
         self.graphicsView.setRenderHint(QPainter.RenderHint.Antialiasing)
         self.graphicsView.show()
 
-        image_number = Main.image_index + 1
-        total_image_number = len(Main.image_array)
-        self.label_5.setText("Image: "+ str(image_number) + " / " + str(total_image_number))
-
-    def set_image_last(self):
-        # self.eventLog.appendPlainText('Clicked on Last Button')
-        scene = QGraphicsScene(0, 0, 0, 0)
-
-        image_index = len(Main.image_array) - 1
-        image_prop = Main.image_array[image_index]
-        self.imgTitle.setText(image_prop[0].split(',')[0])
-        image_path = image_prop[0].split(',')[1]
-        pixmap = QPixmap(image_path)
-        pixmapitem = scene.addPixmap(pixmap)
-
-        self.graphicsView.setScene(scene)
-        self.graphicsView.setRenderHint(QPainter.RenderHint.Antialiasing)
-        self.graphicsView.show()
-
-        image_number = Main.image_index + 1
+        image_number = len(Main.image_array)
         total_image_number = len(Main.image_array)
         self.label_5.setText("Image: "+ str(image_number) + " / " + str(total_image_number))
 
